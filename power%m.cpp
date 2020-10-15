c658f3f82d9dc068f39624c21244f4c858786827
Sanchit Gupta <56361917+Major2001@users.noreply.github.com>
2020-10-15 15:48:08 +0530

    Add files via upload

---
+++ power%m.cpp
@@ -0,0 +1,18 @@
+ll power(ll x, ll y)
+{
+    ll res = 1;
+    x = x % mod;
+
+    if (x == 0)
+        return 0;
+
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % mod;
+
+        y = y >> 1;
+        x = (x * x) % mod;
+    }
+    return res;
+}
